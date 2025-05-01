37e4b2a39e962efddc07eb9b643af56504b3f78b
James Barnes <jbarnes.hello@gmail.com>
2025-04-30 20:44:23 -0400

    Refactor for Astro

---
+++ src/assets/index.js
@@ -0,0 +1,33 @@
+import gallery1 from "./images/gallery-1.jpg";
+import gallery2 from "./images/gallery-2.jpg";
+import gallery3 from "./images/gallery-3.jpg";
+import gallery4 from "./images/gallery-4.jpg";
+import gallery5 from "./images/gallery-5.jpg";
+import gallery6 from "./images/gallery-6.jpg";
+import gallery7 from "./images/gallery-7.jpg";
+import gallery8 from "./images/gallery-8.jpg";
+import gallery9 from "./images/gallery-9.jpg";
+import gallery10 from "./images/gallery-10.jpg";
+import event1 from "./images/event-1.jpg";
+import event2 from "./images/event-2.jpg";
+import event3 from "./images/event-3.jpg";
+import tutorial1 from "./images/tutorial-1.jpg";
+import tutorial2 from "./images/tutorial-2.jpg";
+import tutorial3 from "./images/tutorial-3.jpg";
+
+
+export {
+  gallery1,
+  gallery2,
+  gallery3,
+  gallery4,
+  gallery5,
+  gallery6,
+  gallery7,
+  gallery8,
+  gallery9,
+  gallery10,
+};
+  
+export { event1, event2, event3 };
+export { tutorial1, tutorial2, tutorial3 };
